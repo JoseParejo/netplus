0b717915686df7c368fb961179128ff8d456c756
salas <salas@trajano.us.es>
2019-01-22 12:20:06 +0100

    Fix

--- proyecto/first.cc
+++ proyecto/first.cc
@@ -22,16 +22,18 @@
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
+NS_LOG_COMPONENT_DEFINE ("first");
 
 int
 main (int argc, char *argv[])
 {
+  NS_LOG_INFO("Comienza");
+  DataRate    tasaEnvio ("100kb/s");
 //Control de línea de comandos
   CommandLine cmd;
   cmd.Parse (argc, argv);
 //Resolucion en minutos o segundos?
-  Time::SetResolution (Time::M);
+  Time::SetResolution (Time::MIN);
 //2 Nodos por contenedor, 1 nodo esta en los 3 contenedores.
   NodeContainer nodosTV;
   NodeContainer nodosPe;
@@ -41,6 +43,9 @@ main (int argc, char *argv[])
   nodosSe.Add(nodosTV.Get(0));
   nodosPe.Create (1);
   nodosSe.Create (1);
+
+  NS_LOG_INFO("Toca las VA");
+
 //Para el pico de trafico de television
   Ptr <NormalRandomVariable> pton_tv = CreateObject<NormalRandomVariable> ();
   pton_tv->SetAttribute("Mean", DoubleValue(8.0));
@@ -61,6 +66,7 @@ main (int argc, char *argv[])
 	Preparacion del escenario
 	***********************
 */
+  NS_LOG_INFO("Comienza escenario");
 //Canal P2P
   PointToPointHelper pointToPoint;
   pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
@@ -68,16 +74,17 @@ main (int argc, char *argv[])
 //Instalamos los nodos
   NetDeviceContainer devicesTV;
   devicesTV = pointToPoint.Install (nodosTV);
-  NetDeviceContainer devicesPe;
-  devicesSe = pointToPoint.Install (nodosPe);
   NetDeviceContainer devicesSe;
-  devicesPe = pointToPoint.Install (nodosSe);
+  devicesSe = pointToPoint.Install (nodosSe);
+  NetDeviceContainer devicesPe;
+  devicesPe = pointToPoint.Install (nodosPe);
+  NS_LOG_INFO("IP");
 
 //Instalamos la pila IP
   InternetStackHelper stack;
   stack.Install (nodosTV);
-  stack.Install (nodosPe);
-  stack.Install (nodosSe);
+  stack.Install (nodosPe.Get(1));
+  stack.Install (nodosSe.Get(1));
 //Asignamos direcciones a servicio TV
   Ipv4AddressHelper address;
   address.SetBase ("10.1.1.0", "255.255.255.0");
@@ -90,48 +97,53 @@ main (int argc, char *argv[])
   Ipv4InterfaceContainer interfacesSe = address.Assign (devicesSe);
 
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+  NS_LOG_INFO("Comienza servers");
 
 //Creamos la aplicacion OnOff UDP para Series
   OnOffHelper serverSe ("ns3::UdpSocketFactory", 
-  	Address (InetSocketAddres(interfacesSe.GetAddress(0) ,9)));
-  serverSe.SetAttribute("DataRate", DataRateValue());
-  serverSe.SetAttribute("PacketSize", UintegerValue());
+  	Address (InetSocketAddress(interfacesSe.GetAddress(0) ,9)));
+  serverSe.SetAttribute("DataRate", DataRateValue(tasaEnvio));
+  serverSe.SetAttribute("PacketSize", UintegerValue(500));
   serverSe.SetAttribute("OnTime", PointerValue(pton_stream));
   serverSe.SetAttribute("OffTime", PointerValue(ptoff_stream));  
 
 //Creamos la aplicacion OnOff UDP para Peliculas  
   OnOffHelper serverPe ("ns3::UdpSocketFactory", 
-  	Address (InetSocketAddres(interfacesPe.GetAddress(0) ,10)));
-  serverPe.SetAttribute("DataRate", DataRateValue());
-  serverPe.SetAttribute("PacketSize", UintegerValue());
+  	Address (InetSocketAddress(interfacesPe.GetAddress(0) ,10)));
+  serverPe.SetAttribute("DataRate", DataRateValue(tasaEnvio));
+  serverPe.SetAttribute("PacketSize", UintegerValue(500));
   serverPe.SetAttribute("OnTime", PointerValue(pton_stream));
   serverPe.SetAttribute("OffTime", PointerValue(ptoff_stream));
   
 //Creamos la aplicacion OnOff TCP para TV
   OnOffHelper serverTV ("ns3::TcpSocketFactory", 
-  	Address (InetSocketAddres(interfacesTV.GetAddress(0) ,11)));
-  serverTV.SetAttribute("DataRate", DataRateValue());
-  serverTV.SetAttribute("PacketSize", UintegerValue());
+  	Address (InetSocketAddress(interfacesTV.GetAddress(0) ,11)));
+  serverTV.SetAttribute("DataRate", DataRateValue(tasaEnvio));
+  serverTV.SetAttribute("PacketSize", UintegerValue(500));
   serverTV.SetAttribute("OnTime", PointerValue(pton_tv));
   serverTV.SetAttribute("OffTime", PointerValue(ptoff_tv));
-  //serverTV.SetAttribute("MaxBytes", UintegerValue());
-
+  //serverTV.SetAttribute("MaxBytes", ));
 //Instalamos las aplicaciones
   ApplicationContainer appServerSe = serverSe.Install (nodosSe.Get (1));
   ApplicationContainer appServerPe = serverPe.Install (nodosPe.Get (1));
   ApplicationContainer appServerTV = serverTV.Install (nodosTV.Get (1));
 
 //Programamos comienzo y final de las aplicaciones
-  //serverApps.Start (Hours (0.0));
-  //serverApps.Stop (Hours (12.0));
+  appServerPe.Start (Hours (0.0));
+  appServerSe.Start (Hours (0.0));
+  appServerTV.Start (Hours (0.0));
+  appServerPe.Stop (Hours (1.0));
+  appServerSe.Stop (Hours (1.0));
+  appServerTV.Stop (Hours (1.0));
+  NS_LOG_INFO("Comienza clients");
 
 //Creamos las aplicaciones PacketSink correspondientes a las OnOff
   PacketSinkHelper clientTV ("ns3::TcpSocketFactory", 
-  	Address (InetSocketAddres(interfacesTV.GetAddress(1), 11)));
+  	Address (InetSocketAddress(interfacesTV.GetAddress(1), 11)));
   PacketSinkHelper clientSe ("ns3::UdpSocketFactory", 
-  	Address (InetSocketAddres(interfacesSe.GetAddress(1), 11)));
+  	Address (InetSocketAddress(interfacesSe.GetAddress(1), 11)));
   PacketSinkHelper clientPe ("ns3::UdpSocketFactory", 
-  	Address (InetSocketAddres(interfacesPe.GetAddress(1), 11)));
+  	Address (InetSocketAddress(interfacesPe.GetAddress(1), 11)));
 
 //Instalamos las aplicaciones
   ApplicationContainer appClientSe = clientSe.Install (nodosSe.Get (0));
@@ -139,8 +151,17 @@ main (int argc, char *argv[])
   ApplicationContainer appClientTV = clientTV.Install (nodosTV.Get (0));
 
 //Programamos comienzo y final de las aplicaciones
-  //clientApps.Start (Hours (8.0));
-  //clientApps.Stop (Hours (10.0));
+  appClientTV.Start (Hours (0.3));
+  appClientPe.Start (Hours (0.3));
+  appClientSe.Start (Hours (0.3));
+  appClientSe.Stop (Hours (0.8));
+  appClientPe.Stop (Hours (0.8));
+  appClientTV.Stop (Hours (0.8));
+
+  pointToPoint.EnablePcap("pruebaSerie", devicesSe.Get(1), true);
+  pointToPoint.EnablePcap("pruebaPelicula", devicesPe.Get(1), true);
+  pointToPoint.EnablePcap("pruebaTv", devicesTV.Get(1), true);
+  NS_LOG_INFO("Comienza la simulacion");
 
   Simulator::Run ();
   Simulator::Destroy ();
